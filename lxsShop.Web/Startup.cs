1e30535a3ca16ce7c39d3f1c03c4aa214450b32a
longzhang <pig1717@gmail.com>
2020-02-01 20:01:15 +0800

    初步完成登录页面

--- lxsShop.Web/Startup.cs
+++ lxsShop.Web/Startup.cs
@@ -2,19 +2,33 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FineUICore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace lxsShop.Web
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // FineUI 和 MVC 服务
+            services.AddFineUI(Configuration);
+            services.AddMvc(options => {
+                // 自定义模型绑定（Newtonsoft.Json）
+                options.ModelBinderProviders.Insert(0, new JsonModelBinderProvider());
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +38,24 @@ namespace lxsShop.Web
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            // 静态资源中间件
+            app.UseStaticFiles();
+
+            // FineUI 和 MVC 中间件（确保 UseFineUI 位于 UseMvc 的前面）
+            app.UseFineUI();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "area",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
