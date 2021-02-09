8edbcdabd8b68e4cac6af4eceae9a506ad105838
Long Zhang <pig1717@gmail.com>
2021-02-09 15:23:22 +0800

    初步添加搜索页面

--- lxsShop.Web/Pages/search/Index.cshtml.cs
+++ lxsShop.Web/Pages/search/Index.cshtml.cs
@@ -1,10 +1,6 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using HtmlAgilityPack;
 using lxsShop.NewServices;
@@ -15,10 +11,8 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
-namespace lxsShop.Web.Pages.catalog
+namespace lxsShop.Web.Pages.search
 {
     public class IndexModel : PageModel
     {
@@ -40,17 +34,14 @@ namespace lxsShop.Web.Pages.catalog
         public List<goods_catsViewModel> goods_cats { get; set; }
         public List<goodsViewModel> goods { get; set; }
 
-        [BindProperty(SupportsGet = true)] public int ID { get; set; }
-
-
-        [BindProperty(SupportsGet = true)]
-        public int? pages { get; set; }
+        [BindProperty(SupportsGet = true)] public string keyword { get; set; }
+        [BindProperty(SupportsGet = true)] public int? pages { get; set; }
 
         public long GoodsCount { get; set; }
         public long TotalPages { get; set; }
         public long CurrentPage { get; set; }
         public string CatName { get; set; }
-        public string 纸质产品目录 { get; set; }
+      
 
         public async Task<IActionResult> OnGetAsync()
         {
@@ -58,53 +49,47 @@ namespace lxsShop.Web.Pages.catalog
 
             if (IsAjax(HttpContext.Request))
             {
-               var web = new HtmlWeb();
-               var doc = web.Load(Request.GetDisplayUrl());
-               var titleNode = doc.DocumentNode.SelectSingleNode("//div[@id='products_list']");
-               return Content(titleNode.InnerHtml);
+                var web = new HtmlWeb();
+                var doc = web.Load(Request.GetDisplayUrl());
+                var titleNode = doc.DocumentNode.SelectSingleNode("//div[@id='products_list']");
+                return Content(titleNode.InnerHtml);
             }
 
-            ViewData["Title"] = "产品类别 ";
-            ViewData["keywords"] = "产品类别 ";
+            ViewData["Title"] = string.Format("搜索'{0}'相关产品", keyword);
+            
+
+           
 
             ViewData["keywords"] = await _sysconfigsserver.GetKeyAsync("keywords");
             ViewData["description"] = await _sysconfigsserver.GetKeyAsync("description");
 
-            纸质产品目录 = await _sysconfigsserver.GetKeyAsync("纸质产品目录");
+          
 
-            var post = await _goodscatsserver.GetPagesAsync(new PageParm() {limit = 20, attr = 0,where = "parentId"});
+            var post = await _goodscatsserver.GetPagesAsync(new PageParm() { limit = 20, attr = 0, where = "parentId" });
             goods_cats_top1 = post.data.Items.MapTo<List<goods_catsViewModel>>();
 
-            var post2 = await _goodscatsserver.GetPagesAsync(new PageParm() {limit = 200});
+            var post2 = await _goodscatsserver.GetPagesAsync(new PageParm() { limit = 200 });
             goods_cats = post2.data.Items.MapTo<List<goods_catsViewModel>>();
 
 
-           
-                var postgoods = await _goodserver.GetPagesAsync(new PageParm()
-                    {limit = 20, page = Convert.ToInt16(pages), attr = ID, where = "goodsCatId"});
-                goods = postgoods.data.Items.MapTo<List<goodsViewModel>>();
-                GoodsCount = postgoods.data.TotalItems;
-                TotalPages = postgoods.data.TotalPages;
-                CurrentPage = postgoods.data.CurrentPage;
 
-                if (postgoods.data.Items.Count > 0)
-                {
-                    CatName = postgoods.data.Items[0].catName;
-                }
-                else //判断类别是不是大类  
-                {
-                    var catList = goods_cats.Where(t => t.catId == ID).ToList();
-                    if (catList.Count > 0)
-                    {
-                        CatName = catList[0].catName;
-                    }
-                }
+            var postgoods = await _goodserver.GetPagesAsync(new PageParm()
+            { limit = 20, page = Convert.ToInt16(pages), key   = keyword, where = "search" });
+            goods = postgoods.data.Items.MapTo<List<goodsViewModel>>();
+            GoodsCount = postgoods.data.TotalItems;
+            TotalPages = postgoods.data.TotalPages;
+            CurrentPage = postgoods.data.CurrentPage;
 
-                ViewData["Title"] = CatName + " - " + await _sysconfigsserver.GetKeyAsync("Title");
+            if (postgoods.data.Items.Count > 0)
+            {
+                CatName = postgoods.data.Items[0].catName;
+            }
+          
 
-                return null;
+            ViewData["Title"] = string.Format("搜索'{0}'相关产品", keyword)+ " - " + await _sysconfigsserver.GetKeyAsync("Title");
+
+            return null;
         }
-
 
         public static bool IsAjax(HttpRequest req)
         {
@@ -119,10 +104,7 @@ namespace lxsShop.Web.Pages.catalog
         }
 
 
-       
+
 
     }
-
-
-
 }
