de2a4460ae3aa4be522da97f05a2ddea6284703d
Long Zhang <pig1717@gmail.com>
2021-02-09 11:19:15 +0800

    完成产品展示、优化页数导航条。

--- lxsShop.Web/Pages/fa/Index.cshtml.cs
+++ lxsShop.Web/Pages/fa/Index.cshtml.cs
@@ -16,19 +16,22 @@ namespace lxsShop.Web.Pages.fa
         private readonly Isys_configsServer _sysconfigsserver;
         private readonly Igoods_catsServer _goodscatsserver;
         private readonly IgoodServer _goodserver;
-
+        private readonly IbrandsServer _brandsserver;
 
         public IndexModel(Igoods_catsServer goodscatsserver, Isys_configsServer sysconfigsserver,
-            IgoodServer goodserver)
+            IgoodServer goodserver, IbrandsServer brandsserver)
         {
             _goodscatsserver = goodscatsserver;
             _sysconfigsserver = sysconfigsserver;
             _goodserver = goodserver;
+            _brandsserver = brandsserver;
         }
 
 
         [BindProperty(SupportsGet = true)]
         public int ID { get; set; }
+        public string TopcatName { get; set; }
+        public string brandImg { get; set; }
 
         public goodsViewModel  goods { get; set; }
 
@@ -39,12 +42,37 @@ namespace lxsShop.Web.Pages.fa
             ViewData["Title"] = "没有找到该产品";
             ViewData["keywords"] = "产品类别 ";
             ViewData["description"] = await _sysconfigsserver.GetKeyAsync("description");
+            ViewData["Tel"] = await _sysconfigsserver.GetKeyAsync("tel");
 
+            
 
             if (post.data.Items.Count > 0)
             {
                 ViewData["Title"] = post.data.Items[0].goodsName + " - " + post.data.Items[0].catName + " - " + await _sysconfigsserver.GetKeyAsync("Title");
                 goods = post.data.Items[0];
+
+                var postcat = await _goodscatsserver.GetPagesAsync(new PageParm() { limit = 20, attr =Convert.ToInt32(post.data.Items[0].goodsCatId), where = "catid" });
+                if (postcat.data.Items.Count > 0)
+                {
+                    var parentId = postcat.data.Items[0].parentId;
+                    postcat = await _goodscatsserver.GetPagesAsync(new PageParm() { limit = 20, attr = Convert.ToInt32(parentId), where = "parentId" });
+                    if (postcat.data.Items.Count > 0)
+                    {
+                        TopcatName= postcat.data.Items[0].catName;
+                    }
+                }
+
+                if (goods.brandId > 0)
+                {
+                   var brand = await _brandsserver.GetPagesAsync(new PageParm()
+                        { limit = 100,  key = goods.brandId.ToString() });
+                   if (brand.data.Items.Count > 0)
+                   {
+                       brandImg = "/uploads/Logo/" + brand.data.Items[0].brandImg;
+                   }
+                }
+
+
             }
 
            
