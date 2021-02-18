ccea73be0661e4c2120ce603c62e2093b2376bb1
Long Zhang <pig1717@gmail.com>
2021-02-18 08:54:19 +0800

    初步支持搜索意见

---
+++ lxsShop.NewServices/SearchSuggest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lxsShop.NewServices
+{
+  public  class SearchSuggest
+    {
+            public string name { get; set; }
+            public string href { get; set; }
+    }
+}
---
+++ lxsShop.Web/Pages/API/search_suggestController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lxsShop.NewServices;
+using lxsShop.NewServices.Interfaces;
+
+namespace lxsShop.Web.Pages.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class search_suggestController : ControllerBase
+    {
+
+        private  IgoodServer _goodserver;
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> search_suggest([FromForm] string keyword)
+        {
+            // _goodserver = goodserver;
+            //
+            // var postgoods = await _goodserver.GetPagesAsync(new PageParm()
+            // {
+            //     key = keyword,
+            //     where = "search"
+            // });
+
+            string message = string.Format("搜索[{0}]找到{1}个产品", keyword,15);
+
+            var t1 = new SearchSuggest() { href = "/search/"+ keyword, name = message };
+            return new JsonResult(new List<SearchSuggest> { t1 });
+        }
+
+ 
+
+    }
+}
